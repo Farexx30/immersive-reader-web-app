6784afd24bd03f78220f3dca16d01b4486bb543a
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-01-30 22:49:00 +0100

    Init: The implementation.

---
+++ ImmersiveReaderWebAppModule4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImmersiveReaderWebAppModule4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
